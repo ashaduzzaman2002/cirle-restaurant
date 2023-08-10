9ae05189d216458fc110b980f91e1aa6b345f643
ProgrammerAlok <alokpaul5584@gmail.com>
2023-08-10 07:17:43 +0530

    signup page done

--- src/screens/auth/Signup.jsx
+++ src/screens/auth/Signup.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../layout/Layout'
+import './signup.css'
 
 const Signup = () => {
+
+  const [user, setUser] = useState({name: '', mobile: ''})
+
   return (
     <Layout>
-        <div className='container cm'>Signup</div>
+      <div className="signup_container">
+
+        <div className="signup_left_logo"></div>
+
+        <div className="box signup_box">
+          <h1>Confirm Your Order</h1>
+          <input type="text" name='name' placeholder='Enter Your Name' value={user.name} onChange={e=>setUser({...user, name: e.target.value})} />
+          <input type="number" name='mobile' placeholder='Enter Mobile Number' value={user.mobile} onChange={e => setUser({...user, mobile: e.target.value})}/>
+          <div className="btn btn1">Confirm</div>
+        </div>
+
+        <div className="signup_right_logo"></div>
+
+      </div>
     </Layout>
   )
 }
